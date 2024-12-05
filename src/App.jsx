5db158cb23e5646e3c85dcaaadd7e71cecdf49ff
alirrah <alir.rah81@gmail.com>
2024-12-05 18:10:10 +0330

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
-import Routes from "./routes.jsx";
-import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import ProtectedRoutes from "./utils/ProtectedRoutes.jsx";
 
-const App = () => <Routes/>
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* route no need to be authenticated */}
+                <Route path="/login" element={<p>login</p>}/>
+
+                {/* route need to be authenticated */}
+                <Route element={<ProtectedRoutes/>}>
+                    <Route path="/" exact element={<p>dashboard</p>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
 
 export default App
