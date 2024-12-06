25c17cee20417bc24300a3fc40c929930415636a
alirrah <alir.rah81@gmail.com>
2024-12-06 08:55:54 +0330

    change font to vazir

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import ProtectedRoutes from "./utils/ProtectedRoutes.jsx";
+import Login from "./containers/Login/";
+import "./App.css";
+import {ConfigProvider} from "antd";
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                {/* route no need to be authenticated */}
-                <Route path="/login" element={<p>login</p>}/>
+        <ConfigProvider theme={{
+            token: {
+                fontFamily: "Vazir"
+            }
+        }}>
+            <BrowserRouter>
+                <Routes>
+                    {/* route no need to be authenticated */}
+                    <Route path="/login" element={<Login/>}/>
 
-                {/* route need to be authenticated */}
-                <Route element={<ProtectedRoutes/>}>
-                    <Route path="/" exact element={<p>dashboard</p>}/>
-                </Route>
-            </Routes>
-        </BrowserRouter>
+                    {/* route need to be authenticated */}
+                    <Route element={<ProtectedRoutes/>}>
+                        <Route path="/" exact element={<p>dashboard</p>}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </ConfigProvider>
     );
 }
 
