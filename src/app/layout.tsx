ab77b398bac1b18e2c763050b538e81f4cbf8ae8
Alireza Rahmani Samani <alir.rah81@gmail.com>
2024-12-16 13:22:36 +0330

    [ADD] vazirmatn font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
+import { Vazirmatn } from "next/font/google";
 import "./globals.css";
+
+const vazirmatn = Vazirmatn({
+  subsets: ["latin", "arabic"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Tele Market",
@@ -12,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fa" dir="rtl">
+    <html lang="fa" dir="rtl" className={vazirmatn.className}>
       <body>{children}</body>
     </html>
   );
