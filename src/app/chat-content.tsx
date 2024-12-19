f1a55b1424629c3d277c52d6ef31c8e6683dc2cc
alirrah <alir.rah81@gmail.com>
2024-12-19 17:08:06 +0330

    [ADD] dashboard page

---
+++ src/app/chat-content.tsx
@@ -0,0 +1,5 @@
+const ChatContent = () => {
+    return (<></>);
+}
+
+export default ChatContent;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,34 @@
-const Dashboard = () => {
-  return <></>;
-};
+"use client";
 
+import { Layout, theme } from 'antd';
+import ChatContent from "./chat-content";
+import ChatHeader from "./chat-header";
+import ChatMenu from "./chat-menu";
+
+const { Header, Content, Sider } = Layout;
+
+const Dashboard = () => {
+  const { token: { colorBgContainer } } = theme.useToken();
+
+  return (
+    <Layout style={{ height: '100dvh' }}>
+      <Sider theme="light" style={{ padding: "0.8rem 0" }}><ChatMenu /></Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        ><ChatHeader /></Header>
+        <Content
+          style={{
+            margin: '1.6rem',
+          }}
+        >
+          <ChatContent />
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
 export default Dashboard;
