c83d4691aa768a63e6d373076d751d44177a7cde
alirrah <alir.rah81@gmail.com>
2024-12-19 17:55:01 +0330

    [ADD] dashboard menu

--- src/app/chat-menu.tsx
+++ src/app/chat-menu.tsx
@@ -1,5 +1,138 @@
-const ChatMenu = () => {
-    return (<></>);
+import { Avatar, Button, Flex, List, Typography } from "antd";
+
+const items = [
+  {
+    key: 1,
+    label: "گروه یک"
+  },
+  {
+    key: 2,
+    label: "گروه دو"
+  },
+  {
+    key: 3,
+    label: "گروه سه"
+  },
+  {
+    key: 4,
+    label: "گروه چهار"
+  },
+  {
+    key: 5,
+    label: "گروه پنج"
+  },
+  {
+    key: 6,
+    label: "گروه پنج"
+  },
+  {
+    key: 7,
+    label: "گروه پنج"
+  },
+  {
+    key: 8,
+    label: "گروه پنج"
+  },
+  {
+    key: 9,
+    label: "گروه پنج"
+  }, {
+    key: 10,
+    label: "گروه پنج"
+  }, {
+    key: 11,
+    label: "گروه پنج"
+  }, {
+    key: 12,
+    label: "گروه پنج"
+  }, {
+    key: 13,
+    label: "گروه پنجreteruoooooooooooooooooooooooooooooooooooooo"
+  }, {
+    key: 14,
+    label: "گروه پنج"
+  }, {
+    key: 15,
+    label: "گروه پنج"
+  }, {
+    key: 16,
+    label: "گروه پنج"
+  }, {
+    key: 17,
+    label: "گروه پنج"
+  }, {
+    key: 18,
+    label: "گروه پنج"
+  }, {
+    key: 19,
+    label: "گروه پنج"
+  }, {
+    key: 20,
+    label: "گروه پنج"
+  }, {
+    key: 21,
+    label: "گروه پنج"
+  }, {
+    key: 22,
+    label: "گروه پنج"
+  }, {
+    key: 23,
+    label: "گروه پنج"
+  }, {
+    key: 24,
+    label: "گروه پنج"
+  }, {
+    key: 25,
+    label: "گروه پنج"
+  }, {
+    key: 26,
+    label: "گروه پنج"
+  }, {
+    key: 27,
+    label: "گروه پنج"
+  }, {
+    key: 28,
+    label: "گروه پنج"
+  }, {
+    key: 29,
+    label: "گروه پنج"
+  }, {
+    key: 30,
+    label: "گروه پنج"
+  }, {
+    key: 31,
+    label: "گروه پنج"
+  }, {
+    key: 32,
+    label: "گروه پنج"
+  }, {
+    key: 33,
+    label: "گروه پنج"
+  },
+
+];
+
+const ChatMenu = ({ selected, setSelected }) => {
+  return (
+    <List
+      dataSource={items}
+      renderItem={(item) => (
+        <Button type="text" size="large" key={item.key} color={item.key === selected ? "primary" : "default"}
+                variant={item.key === selected ? "filled" : "text"} style={{ width: "100%", margin: "0.2rem 0" }}
+                onClick={() => {
+                  setSelected(item.key);
+                }}>
+          <Flex align="center" gap="small" style={{ width: "100%" }}>
+            <Avatar shape="square" src="" alt="" style={{ minWidth: "3.2rem" }}/>
+            <Typography.Paragraph ellipsis={{
+              rows: 1,
+            }} style={{ color: "inherit", marginBottom: 0 }}>{item.label}</Typography.Paragraph>
+          </Flex>
+        </Button>
+      )}
+      style={{ height: "100%", overflowX: "auto", padding: "0.2rem 0" }}
+    />
+  );
 }
 
 export default ChatMenu;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,18 @@ import { Layout, theme } from 'antd';
 import ChatContent from "./chat-content";
 import ChatHeader from "./chat-header";
 import ChatMenu from "./chat-menu";
+import { useState } from "react";
 
 const { Header, Content, Sider } = Layout;
 
 const Dashboard = () => {
+  const [selected, setSelected] = useState(1);
+
   const { token: { colorBgContainer } } = theme.useToken();
 
   return (
     <Layout style={{ height: '100dvh' }}>
-      <Sider theme="light" style={{ padding: "0.8rem 0" }}><ChatMenu /></Sider>
+      <Sider theme="light"><ChatMenu selected={selected} setSelected={setSelected} /></Sider>
       <Layout>
         <Header
           style={{
