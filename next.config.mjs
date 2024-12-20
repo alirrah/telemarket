1c337c5dc3cffb46ab7501a2e16a3d72bc3ee6cc
alirrah81 <82468300+alirrah@users.noreply.github.com>
2024-12-20 15:59:36 +0330

    deply (#5)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/telemarket",
+    output: "export",
+    reactStrictMode: true
+};
 
 export default nextConfig;
