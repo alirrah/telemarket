efa918a3c9a85e2c37bba418f794801e2776dc8e
alirrah <alir.rah81@gmail.com>
2024-12-27 17:40:16 +0330

    type error

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/telemarket",
-  // output: "export",
-  // reactStrictMode: true
+  output: "export",
+  reactStrictMode: true
 };
 
 export default nextConfig;
