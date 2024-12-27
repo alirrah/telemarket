0dd34a709b7af4f6286a37d6b3d735ac5a15f104
alirrah <alir.rah81@gmail.com>
2024-12-27 10:25:58 +0330

    [ADD] call api

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+import { useState } from "react";
+
 import {
   Layout,
   Card,
@@ -11,31 +16,57 @@ import {
   Space,
   Typography,
   theme,
+  notification,
 } from "antd";
 import { MailOutlined, LockOutlined } from "@ant-design/icons";
-import { useState } from "react";
-import Link from "next/link";
+
+import axios from "axios";
 
 const { Content } = Layout;
 const { Title } = Typography;
 
 const Login = () => {
   const [loading, setLoading] = useState<boolean>(false);
+  const [api, contextHolder] = notification.useNotification();
+  const router = useRouter();
 
   const {
     token: { colorBgLayout, colorTextDescription },
   } = theme.useToken();
 
-  const onFinish = async (values: { username: string; password: string }) => {
+  const onFinish = async (values: { email: string; password: string }) => {
     setLoading(true);
-    console.log(values);
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
+    axios
+      .post(
+        "https://alisadeqi.pythonanywhere.com/api/account/login/",
+        {
+          email: values.email,
+          password: values.password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      )
+      .then((res) => {
+        localStorage.setItem("token", JSON.stringify(res.data));
+        router.push("/");
+      })
+      .catch((err) => {
+        api.error({
+          message: "خطا در ارسال درخواست",
+          description: err.message,
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   return (
     <Content style={{ height: "100dvh", backgroundColor: colorBgLayout }}>
+      {contextHolder}
       <Flex align="center" justify="center" style={{ height: "100%" }}>
         <Card styles={{ body: { paddingInline: 0 } }}>
           <Space size="large" direction="vertical">
